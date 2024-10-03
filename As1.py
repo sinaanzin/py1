fa2971d0ab58921509d4c25260ee4e174c2b6861
sinaanzin <128033761+sinaanzin@users.noreply.github.com>
2024-10-03 15:37:21 +0530

    Add files via upload

---
+++ As1.py
@@ -0,0 +1,43 @@
+print ("Sinaan ST1007 Zin@gmail.com")
+
+print("Sinaan\nST1007\nZin@gmail.com")
+
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+
+for i in range(1,6):
+    print(i)
+
+print('"SDK" stands for "Software Development Kit", whereas "IDE" stands for "Integrated Development Environment"')
+
+print("python is an \"awsome\"language")
+print("python\n\t2023")
+print('I\'m from Entri.\b')
+print("\65")
+print("\x65")
+print("Entri\n2023")
+print("Entri\b2023")
+print("Entri*2023",end="\b\b\b\b")
+
+print("\n")
+
+num =23
+textnum ="57"
+decimal =98.3
+total =num +int(textnum)+ decimal
+print(type(total))
+
+days =365
+hours =24
+mins = 60
+total = days*hours*mins
+print(total)
+
+l=input("Enter your name")
+print(f"Hi {l},Welcome to the python programming")
+
+
---
+++ PoundsToDollars.py
@@ -0,0 +1,3 @@
+pounds = float(input("Enter amount in pounds: "))
+dollars = pounds * 1.32
+print(f"£ {pounds} are $ {dollars:}")
